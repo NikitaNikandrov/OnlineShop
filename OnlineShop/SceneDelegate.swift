207c2174255cd6da7df5a304ea5bad27d68c662c
NikitaNikandrov <nnikandroff@yandex.ru>
2023-03-13 20:28:17 +0300

    1. Added RootViewController 2. Configure SceneDelegate 3. Added UIColor extension

--- OnlineShop/SceneDelegate.swift
+++ OnlineShop/SceneDelegate.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    static weak var shared: SceneDelegate?
+    
+    var rootViewController: RootViewController {
+        return window!.rootViewController as! RootViewController
+    }
+    
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        Self.shared = self
+        
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = RootViewController()
+        window?.makeKeyAndVisible()
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
