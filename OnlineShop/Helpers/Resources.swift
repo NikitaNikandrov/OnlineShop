d9a50357034a7ee8a0bdf17824d6ca6d664f440b
NikitaNikandrov <nnikandroff@yandex.ru>
2023-03-14 00:06:15 +0300

    Added password text field

--- OnlineShop/Helpers/Resources.swift
+++ OnlineShop/Helpers/Resources.swift
@@ -10,16 +10,20 @@ import UIKit
 enum Resources {
     
     enum Colors {
-      static var black = UIColor(hexString: "161826")
-        static var white = UIColor(hexString: "FCFCFE")
+        static var black = UIColor(hexString: "161826")
+        static var white = UIColor(hexString: "FAF9FF")
+        static var lightGrey = UIColor(hexString: "E8E8E8")
+        static var grey = UIColor(hexString: "7B7B7B")
     }
     
     enum Fonts {
         static var montserratBold = "Montserrat-Bold"
-        static var montserratExtraLight = "Montserrat-ExtraLight"
+        static var montserratRegular = "Montserrat-Regular"
     }
     
     enum LoginStrings {
         static var titleString = "Welcome back"
+        static var firstNameTextHolderString = "First name"
+        static var passwordTextHolderString = "Password"
     }
 }
--- OnlineShop/Views/LoginViewController.swift
+++ OnlineShop/Views/LoginViewController.swift
@@ -19,6 +19,34 @@ class LoginViewController: UIViewController {
         return label
     }()
     
+    private let firstNameTextField: UITextField = {
+        var textField = UITextField()
+        textField.attributedPlaceholder = NSAttributedString( string: Resources.LoginStrings.firstNameTextHolderString,
+                                                              attributes: [NSAttributedString.Key.foregroundColor: Resources.Colors.grey,
+                                                                           .font: UIFont(name: Resources.Fonts.montserratRegular,
+                                                                                         size: 8.7) ?? UIFont.systemFont(ofSize: 8.7, weight:           .regular)])
+        textField.textAlignment = .center
+        textField.backgroundColor = Resources.Colors.lightGrey
+        textField.textColor = Resources.Colors.grey
+        textField.layer.cornerRadius = 15.5
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        var textField = UITextField()
+        textField.attributedPlaceholder = NSAttributedString( string: Resources.LoginStrings.passwordTextHolderString,
+                                                              attributes: [NSAttributedString.Key.foregroundColor: Resources.Colors.grey,
+                                                                           .font: UIFont(name: Resources.Fonts.montserratRegular,
+                                                                                         size: 8.7) ?? UIFont.systemFont(ofSize: 8.7, weight:           .regular)])
+        textField.textAlignment = .center
+        textField.backgroundColor = Resources.Colors.lightGrey
+        textField.textColor = Resources.Colors.grey
+        textField.layer.cornerRadius = 15.5
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
@@ -29,16 +57,36 @@ class LoginViewController: UIViewController {
         
         view.addSubview(titleLabel)
         setTitleLabelConstraints()
+        
+        view.addSubview(firstNameTextField)
+        setFirstNameTextFieldConstraints()
+        
+        view.addSubview(passwordTextField)
+        setPasswordTextFieldConstraints()
     }
 }
 
 extension LoginViewController {
-   
-    private func setTitleLabelConstraints(){
+    
+    private func setTitleLabelConstraints() {
         titleLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
         titleLabel.widthAnchor.constraint(equalToConstant: 250).isActive = true
         titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 168.5).isActive = true
         titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+    }
+    
+    private func setFirstNameTextFieldConstraints() {
+        firstNameTextField.heightAnchor.constraint(equalToConstant: 31).isActive = true
+        firstNameTextField.widthAnchor.constraint(equalToConstant: 301).isActive = true
+        firstNameTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 84).isActive = true
+        firstNameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+    }
+    
+    private func setPasswordTextFieldConstraints() {
+        passwordTextField.heightAnchor.constraint(equalToConstant: 31).isActive = true
+        passwordTextField.widthAnchor.constraint(equalToConstant: 301).isActive = true
+        passwordTextField.topAnchor.constraint(equalTo: firstNameTextField.bottomAnchor, constant: 36.4).isActive = true
+        passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
     }
 }
 
