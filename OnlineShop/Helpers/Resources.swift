f13e1702d22400c1c80770da2d721f9713af231b
NikitaNikandrov <nnikandroff@yandex.ru>
2023-03-13 22:35:39 +0300

    1. Created title label to LoginViewController 2. Load custom fonts to project

---
+++ OnlineShop/Helpers/Resources.swift
@@ -0,0 +1,25 @@
+//
+//  Resources.swift
+//  OnlineShop
+//
+//  Created by Никита on 13.03.2023.
+//
+
+import UIKit
+
+enum Resources {
+    
+    enum Colors {
+      static var black = UIColor(hexString: "161826")
+        static var white = UIColor(hexString: "FCFCFE")
+    }
+    
+    enum Fonts {
+        static var montserratBold = "Montserrat-Bold"
+        static var montserratExtraLight = "Montserrat-ExtraLight"
+    }
+    
+    enum LoginStrings {
+        static var titleString = "Welcome back"
+    }
+}
--- OnlineShop/Views/LoginViewController.swift
+++ OnlineShop/Views/LoginViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    //MARK: Constants
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = Resources.LoginStrings.titleString
+        label.textColor = Resources.Colors.black
+        label.font = UIFont(name: Resources.Fonts.montserratBold, size: 20.3)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureViewController()
     }
+    
+    private func configureViewController() {
+        view.backgroundColor = Resources.Colors.white
+        
+        view.addSubview(titleLabel)
+        setTitleLabelConstraints()
+    }
+}
 
-
+extension LoginViewController {
+   
+    private func setTitleLabelConstraints(){
+        titleLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        titleLabel.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 168.5).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+    }
 }
 
