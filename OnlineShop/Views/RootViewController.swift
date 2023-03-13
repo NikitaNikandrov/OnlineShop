1cad9c9ca2fbd500d1d1f9498e1caa8b0b5b00c3
NikitaNikandrov <nnikandroff@yandex.ru>
2023-03-13 20:09:47 +0300

    Initial commit

---
+++ OnlineShop/Views/RootViewController.swift
@@ -0,0 +1,58 @@
+//
+//  RootViewController.swift
+//  OnlineShop
+//
+//  Created by Никита on 13.03.2023.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    //MARK: variables
+    private var current: UIViewController
+    
+    //MARK: Init
+    init() {
+        self.current = LoginViewController()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addChild(current)
+        current.view.frame = view.bounds
+        view.addSubview(current.view)
+        current.didMove(toParent: self)
+    }
+    
+    //MARK: Methods
+ /*   func showLoginScreen() {
+        let new = UINavigationController(rootViewController: LogInViewController())
+        addChild(new)
+        new.view.frame = view.bounds
+        view.addSubview(new.view)
+        new.didMove(toParent: self)
+        current.willMove(toParent: nil)
+        current.view.removeFromSuperview()
+        current.removeFromParent()
+        current = new
+    }
+    
+    func showMainScreen() {
+        let new = TabBarController()
+        addChild(new)
+        new.view.frame = view.bounds
+        view.addSubview(new.view)
+        new.didMove(toParent: self)
+        current.willMove(toParent: nil)
+        current.view.removeFromSuperview()
+        current.removeFromParent()
+        current = new
+    }*/
+}
